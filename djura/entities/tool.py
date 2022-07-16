46ed13f09c74e300a03f3ee842711afd0d3a4443
Nazar Vinnichuk <nazar@vinnich.uk>
2022-07-16 14:20:14 +0300

    Improve typing of the Tool entity.

--- djura/entities/tool.py
+++ djura/entities/tool.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import Type, TypeVar
 
 from djura.entities.project import Project
 from djura.entities.version import Version
 
+T = TypeVar("T")
+
 
 class Tool(ABC):
-    @staticmethod
+    @classmethod
     @abstractmethod
-    def get_required_instances(project: Project) -> frozenset["Tool"]:
+    def get_required_instances(cls: Type[T], project: Project) -> frozenset[T]:
         ...
 
     @abstractmethod
